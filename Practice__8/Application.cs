8c73b4a3cc5c56912a2efb65a951df1b9c4b3d80
Антон Вальченко <aavalchenko@edu.hse.ru>
2018-06-22 22:22:07 +0500

    Задание выполнено

---
+++ Practice__8/Application.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using InputOutputLib;
+
+namespace Practice__8
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            Graph graph = MakeGraph(Get.Int32("Введите количество вершин графа (нумеруются с нуля): ", 1));
+            string status = IsBipartite(graph) ? "является" : "не является";
+            Console.WriteLine("Заданный граф {0} двудольным.", status);
+            Console.ReadKey();
+        }
+
+        private static Graph MakeGraph(int count)
+        {
+            string[] edges = Get.String("Введите ребра в виде <начало><конец>, разделяя каждое пробелом: ").Split(' ');
+
+            return new Graph(count, edges);
+        }
+
+        private static bool IsBipartite(Graph graph)
+        {
+            int[] marks = new int[graph.Count];
+
+            for (int i = 0; i < graph.Count; i++)
+            {
+                for (int j = i + 1; j < graph.Count; j++)
+                {
+                    if (graph.Matrix[i, j] != 0)
+                    {
+                        if (marks[i] % 2 == marks[j] % 2 && marks[j] != 0)
+                            return false;
+                        marks[j] = marks[i] + 1;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Practice__8/Graph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Practice__8
+{
+    class Graph
+    {
+        public int[,] Matrix { get; }
+        public int Count { get; }
+        public Graph(int count, string[] input)
+        {
+            Count = count;
+            Matrix = new int[count, count];
+            foreach (string edge in input)
+            {
+                Matrix[Convert.ToInt32(Char.ToString(edge[0])), Convert.ToInt32(Char.ToString(edge[1]))] = 1;
+                Matrix[Convert.ToInt32(Char.ToString(edge[1])), Convert.ToInt32(Char.ToString(edge[0]))] = 1;
+            }
+        }
+    }
+}
